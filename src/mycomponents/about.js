466de4004fc5f29ae973a4ea3aa90bacc8819410
Rajat <rajatsharmatopper@gmail.com>
2024-01-25 08:51:52 +0530

    updated intro

--- src/mycomponents/about.js
+++ src/mycomponents/about.js
@@ -3,42 +3,44 @@ export default function About() {
   return (
 
     <div>
-      <h2 className='text-center' style={{ marginTop: "60px" }}>This is about page of my site</h2>
-      <p>
-        **Welcome to Day-Sync - Your Simple and Local Todo List App!**
-
+      <h2 className='text-center' style={{ marginTop: "60px" }}>Welcome to Day-Sync</h2>
+      <h5 className='mx-2' style={{ marginTop: "5rem" }}>
+        <h3> Welcome to Day-Sync - Your Simple and Local Todo List App!</h3>
+      
         Day-Sync is your go-to todo list app that keeps things simple and local. No complicated setups or cloud storage just a straightforward, efficient, and user-friendly interface that puts you in control of your tasks.
+        <br /><br />
 
-        **Key Features:**
-
-        1. **Local Storage Convenience:**
+        
+        <h3>Key Features</h3>
+      
+        1. Local Storage Convenience:<br />
         Day-Sync operates entirely on local storage, ensuring your tasks stay private and secure on your device. No need to worry about external servers or syncing your tasks are always within reach.
-
-        2. **Effortless Task Management:**
+        <br /><br />
+        2. Effortless Task Management:<br />
         Adding, editing, and deleting tasks has never been easier. Day-Sync streamlines the process, allowing you to focus on your tasks rather than navigating through complex menus.
-
-        3. **Sleek and Intuitive Design:**
+        <br /><br />
+        3. Sleek and Intuitive Design:<br />
         Enjoy a clean and intuitive interface that enhances your user experience. Day-Sync's design is all about simplicity, making task management a breeze without unnecessary distractions.
-
-        4. **Minimalist Color Palette:**
+        <br /><br />
+        4. Minimalist Color Palette:<br />
         Customize your task list with a minimalist color palette. Assign colors to tasks based on your preference, creating a visually appealing yet distraction-free environment.
-
-        5. **Quick Edits on the Fly:**
+        <br /><br />
+        5. Quick Edits on the Fly:<br />
         Need to make changes? Day-Sync lets you quickly edit task details without any complications. It's all about efficiency adapt your to-do list as your day progresses.
-
-        **Why Day-Sync?**
-
-        - **Local Simplicity:**
+        <br /><br />
+        <h3>Why Day-Sync</h3>
+      
+        -Local Simplicity<br />
         Day-Sync embraces simplicity by utilizing local storage, keeping your tasks close at hand without relying on external servers.
-
-        - **Quick and Easy:**
+        <br /><br />
+        -Quick and Easy<br />
         Navigate through your tasks effortlessly with Day-Sync's straightforward design. Spend less time managing your todo list and more time getting things done.
-
-        - **Personalized Minimalism:**
+        <br /><br />
+        -Personalized Minimalism<br />
         Tailor Day-Sync to your liking with a minimalist color palette and clean design. Your tasks, your way.
-
+        <br /><br />
         Day-Sync is the perfect solution for those who appreciate the beauty of simplicity. Use Day-Sync now and experience the joy of managing your tasks without the fuss of cloud storage and security threat because sometimes, local is all you need.
-      </p>
+      </h5>
     </div>
   )
 }
